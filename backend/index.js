2edc6ddebfe87d4a13389be9fde8296c0f7be965
An Lam <anlam@Ans-Laptop.local>
2026-01-05 09:35:49 -0600

    update package.json

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const corsOptions = {
   // Replace this with your actual generated Frontend domain from Railway
-  origin: 'https://https://moviesbigdatalab.up.railway.app/', 
+  origin: 'https://moviesbigdatalab.up.railway.app/', 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, // Allows cookies or authorization headers if needed
   optionsSuccessStatus: 200 
@@ -24,16 +24,17 @@ app.use("/api/titles", titleRoutes);
 
 // 2. Serve Static Files (The React Build)
 // This assumes your React build is in a folder named 'dist' inside 'frontend'
-app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // 3. Fallback Route
 // This is critical for React Router (Single Page Apps) to work on refresh
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 });
 
 // Railway will provide the PORT via environment variables
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
