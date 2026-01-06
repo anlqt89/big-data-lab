a31ecdb57f79819876d536049cdeef9675863584
An Lam <anlam@Ans-Laptop.local>
2026-01-06 12:43:05 -0600

    Complete deploy database to Hetzner, connect database to local NodeJS

--- backend/index.js
+++ backend/index.js
@@ -8,15 +8,15 @@ import titleRoutes from './src/routes/titles.routes.js';
 const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const corsOptions = {
-  // Replace this with your actual generated Frontend domain from Railway
-  origin: 'https://moviesbigdatalab.up.railway.app/', 
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // Allows cookies or authorization headers if needed
-  optionsSuccessStatus: 200 
-};
+// const corsOptions = {
+//   // Replace this with your actual generated Frontend domain from Railway
+//   origin: 'https://moviesbigdatalab.up.railway.app/', 
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   credentials: true, // Allows cookies or authorization headers if needed
+//   optionsSuccessStatus: 200 
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 // 1. API Routes
@@ -24,16 +24,16 @@ app.use("/api/titles", titleRoutes);
 
 // 2. Serve Static Files (The React Build)
 // This assumes your React build is in a folder named 'dist' inside 'frontend'
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 // 3. Fallback Route
 // This is critical for React Router (Single Page Apps) to work on refresh
 app.get('/{*splat}', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
-});
+  res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
+}); 
 
 // Railway will provide the PORT via environment variables
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5002;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,14 +2,40 @@ import pkg from 'pg';
 const { Pool } = pkg;
 import 'dotenv/config';
 
-// On Mac Homebrew, the user is usually your macOS username
-// You can find it by typing 'whoami' in your terminal
-export const pool = new Pool({
+const nodeMode = process.env.NODE_ENV;
+
+// Local Environment
+const dbConfig = {
+  user: process.env.DB_LOCAL_USER, 
+  host: process.env.DB_LOCAL_HOST,
+  database: process.env.DB_LOCAL_NAME,
+  password: process.env.DB_LOCAL_PASSWORD,
+  port: process.env.DB_LOCAL_PORT,
+};
+
+//Production
+if (nodeMode === 'production'){
+  dbConfig =  {
   user: process.env.DB_USER, 
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD, // Homebrew installs usually don't have a default password
+  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  }
+} else{
+  dbConfig =  {
+  user: process.env.DB_TEST_USER, 
+  host: process.env.DB_TEST_HOST,
+  database: process.env.DB_TEST_NAME,
+  password: process.env.DB_TEST_PASSWORD,
+  port: process.env.DB_TEST_PORT,
+  }
+}
+
+// 2. Create the pool using that config
+export const pool = new Pool({
+  ...dbConfig,
+  connectionTimeoutMillis: 5000,
 });
 
-export const  query = (text, params) => pool.query(text, params);
+export const query = (text, params) => pool.query(text, params);
