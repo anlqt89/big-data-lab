6036c18a424bf2e102a0cc4517318f96bb536741
An Lam <anlam@Ans-Laptop.local>
2026-01-10 14:26:06 -0600

    Solve conflict between docker-compose and setup.sh: using loadEnv for Environment setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react'; 
 
-export default defineConfig(() => {
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
 
   return {
     plugins: [react()], 
     server: {
-      port: 5173,
+      port: env.VITE_PORT || 5173,
       proxy: {
         '/api': {
-          target: `http://backend:5001`,
+          target: env.VITE_API_BASE_URL || `http://localhost:5001`,
           changeOrigin: true,
         },
       },
